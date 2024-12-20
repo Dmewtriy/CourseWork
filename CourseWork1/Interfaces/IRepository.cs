8a3d7b43e6f741d838b91ea629e218a72f3acaf7
Dmewtriy <dankin.dm@mail.ru>
2024-12-21 00:12:20 +0700

    Создан интерфейс IRepository
    От IRepository унаслдован абстрактный класс JsonRepository для работы с файлами json

---
+++ CourseWork1/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CourseWork1.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Remove(string key);
+        void Update(T entity);
+        T GetByKey(string key);
+        IEnumerable<T> GetAll();
+    }
+}
