35ac55ea076274e2e5a10b444f49ba425e665655
Dmewtriy <dankin.dm@mail.ru>
2024-12-23 13:46:10 +0700

    Перестановочка и добавлен интерфейса IClientService

---
+++ CourseWork1/Services/IClientService.cs
@@ -0,0 +1,18 @@
+﻿using CourseWork1.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork1.Services
+{
+    public interface IClientService
+    {
+        void RegisterClient(IClient client);
+        void DeleteClient(IClient client);
+        void EditClient(IClient client);
+        IClient GetClientById(int id);
+        IEnumerable<IClient> GetAllClients();
+    }
+}
