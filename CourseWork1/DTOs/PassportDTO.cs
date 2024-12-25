b3d0738a94d330328c911d94a42dc1977b1a9a5f
Dmewtriy <dankin.dm@mail.ru>
2024-12-25 22:58:05 +0700

    добавлен PassportDTO и маппер для пасспорта

---
+++ CourseWork1/DTOs/PassportDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork1.DTOs
+{
+    public class PassportDTO
+    {
+        public string Series { get; set; }
+        public string Number { get; set; }
+        public DateTime IssuedDate{ get; set; }
+        public string IssuedBy{ get; set; }
+    }
+}
---
+++ CourseWork1/Mappers/PassportMapper.cs
@@ -0,0 +1,28 @@
+﻿using CourseWork1.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork1.Mappers
+{
+    public static class PassportMapper
+    {
+        public static PassportDTO ToDTO(IPassport passport)
+        {
+            return new PassportDTO()
+            {
+                Series = passport.Series,
+                Number = passport.Number,
+                IssuedDate = passport.IssuedDate,
+                IssuedBy = passport.IssuedBy,
+            };
+        }
+        public static IPassport ToEntity(PassportDTO passport)
+        {
+            return new RUPassport(passport.Series, passport.Number, passport.IssuedDate,
+                passport.IssuedBy);
+        }
+    }
+}
